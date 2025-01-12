13285ce320d1f5458fba21f7a282516eb37d5db5
Farzane2630 <farzanekazemi8517@gmail.com>
2025-01-12 17:26:14 +0330

    ref: verify-token

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token)
-    return res
-      .status(400)
-      .json({ success: false, message: "Unauthorized - no token provided" });
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!req.headers.authorization) {
+    const error = new Error("unauthorized user!");
+    error.status = 401;
+    return next(error);
+  }
 
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
@@ -15,15 +17,13 @@ const verifyToken = async (req, res, next) => {
         .json({ success: false, message: "Unauthorized - Invalid token" });
 
     req.userId = decodedToken.userId;
-
     next();
   } catch (error) {
-   console.log("error verify token:", error);
-   return res
-     .status(500)
-     .json({ success: false, message: "something went wrong!" });
+    console.log("error verify token:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "something went wrong!" });
   }
 };
 
-
-module.exports = verifyToken
+module.exports = verifyToken;
