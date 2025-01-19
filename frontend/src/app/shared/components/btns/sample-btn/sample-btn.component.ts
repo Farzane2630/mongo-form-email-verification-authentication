c2d48fc96f06370a4f15e171abac704e33cfc184
Farzane2630 <farzanekazemi8517@gmail.com>
2025-01-19 17:26:04 +0330

    ref: btn component

--- frontend/src/app/shared/components/btns/sample-btn/sample-btn.component.ts
+++ frontend/src/app/shared/components/btns/sample-btn/sample-btn.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sample-btn',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sample-btn.component.html',
   // styleUrl: './sample-btn.component.scss'
 })
 export class SampleBtnComponent {
 @Input() title: string = ""
+@Input() route: string = ""
 }
